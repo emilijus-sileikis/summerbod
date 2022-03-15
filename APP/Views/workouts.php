caf7b2787ba9fac53dd7ff10a3535e79d040a63f
Nazar <mamedov.nazar@gmail.com>
2022-03-15 21:04:37 +0200

    workout beginning

---
+++ APP/Views/workouts.php
@@ -0,0 +1,85 @@
+<?php include('partials/menu.php'); ?>
+<!-- Health section start -->
+<link rel="stylesheet" href="/codeigniter4/assets/css/sidenav.css">
+<div id="mySidenav" class="sidenav">
+<ul>
+    <li> <a href="#">Warmups</a> </li>
+	<li> <a href="#">Exercise Techniques</a>
+    <ul>
+	    <li> <a href="#">Calisthenics</a> </li>
+        <li> <a href="#">Powerlifting</a> </li>
+	</ul>
+	<li> <a href="#">Muscle Groups</a>
+    <ul>
+        <li> <a href="#">Traps</a> </li>
+        <li> <a href="#">Traps mid-back</a> </li>
+        <li> <a href="#">Shoulders</a> </li>
+        <li> <a href="#">Chest</a> </li>
+        <li> <a href="#">Biceps</a> </li>
+        <li> <a href="#">Forearms</a> </li>
+        <li> <a href="#">Abdominals</a> </li>
+        <li> <a href="#">Quads</a> </li>
+        <li> <a href="#">Calves</a> </li>
+        <li> <a href="#">Triceps</a> </li>
+        <li> <a href="#">Lats</a> </li>
+        <li> <a href="#">Lower-back</a> </li>
+        <li> <a href="#">Glutes</a> </li>
+        <li> <a href="#">Traps</a> </li>
+    </ul>
+	</li>
+</ul>
+</div>
+<div class="container">
+    <h2 style="color: black; margin-left: 20%" class="text-center">Abdominals Exercises</h2>  <!-- Input muscle group -->
+    <div class="workout-menu-box">
+	<div class="workout-menu-img">
+        <?php include('partials/base64/random-menu.php'); ?>                                  <!-- Input photo by id -->
+    </div>
+        <div class="workout-menu-desc">
+            <h3>Crunches</h3>                                                                 <!-- Input title by id -->
+            <p class="workout-detail">
+            Muscle group: Abdominals </br> </br>                                              <!-- Input details by id -->
+		    Category: Home workout </br> </br>
+		    Difficulty: Begginer 
+            </p>
+        </div>
+	    <div class="clearfix"></div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="workout-menu-box">
+	<div class="workout-menu-img">
+        <?php include('partials/base64/random-menu.php'); ?>                                  <!-- Input photo by id -->
+    </div>
+        <div class="workout-menu-desc">
+            <h3>Crunches</h3>                                                                 <!-- Input title by id -->
+            <p class="workout-detail">
+            Muscle group: Abdominals </br> </br>                                              <!-- Input details by id -->
+		    Category: Home workout </br> </br>
+		    Difficulty: Begginer 
+            </p>
+        </div>
+	    <div class="clearfix"></div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="workout-menu-box">
+	<div class="workout-menu-img">
+        <?php include('partials/base64/random-menu.php'); ?>                                  <!-- Input photo by id -->
+    </div>
+        <div class="workout-menu-desc">
+            <h3>Crunches</h3>                                                                 <!-- Input title by id -->
+            <p class="workout-detail">
+            Muscle group: Abdominals </br> </br>                                              <!-- Input details by id -->
+		    Category: Home workout </br> </br>
+		    Difficulty: Begginer 
+            </p>
+        </div>
+	    <div class="clearfix"></div>
+    </div>
+</div>
+
+<!-- Workouts section end -->
+    <?php include('partials/footer.php'); ?>
