a9f9a3f9ab9faf74d55a3a297d2e28a795952126
Nazar <mamedov.nazar@gmail.com>
2022-04-14 01:43:36 +0300

    automatic exercise display
    automatic loading from database in

--- codeigniter4/app/Views/t_calisthenics.php
+++ codeigniter4/app/Views/t_calisthenics.php
@@ -1,91 +1,59 @@
 <?php include('partials/menu.php'); ?>
+<?php
+include 'db_connnection.php';
+$conn = OpenCon();
+?>
 <!-- Health section start -->
 <link rel="stylesheet" href="<?php echo base_url('assets/css/exercises.css'); ?>">
 <h2 class="text-center workout-header">Calisthenics Exercises</h2>
 
 <div class="diff-menu-container">
  <!-- sorting and stuff, need like 3 more of these to the right -->
-<a href="#" style="color:black;">
-<div class="diff-menu-box diff-menu-box-image1">
-        <h4>Beginner</h4>
-	<div class="clearfix"></div>
-</div>
-</a>
+	<a style="color:black;">
+		<div class="diff-menu-box diff-menu-box-image1">
+			<h4>Sort Difficulty</h4>
+			<div class="clearfix"></div>
+		</div>
+	</a>
 
-<a href="#" style="color:black;">
-<div class="diff-menu-box">
-        <h4>Intermediate</h4>
-	<div class="clearfix"></div>
-</div>
-</a>
-
-<a href="#" style="color:black;">
-<div class="diff-menu-box">
-        <h4>Expert</h4>
-	<div class="clearfix"></div>
-</div>
-</a>
-
-<a href="#" style="color:black;">
-<div class="diff-menu-box">
-        <h4>GymRat</h4>
-	<div class="clearfix"></div>
-</div>
-</a>
-
+	<a style="color:black;">
+		<div class="diff-menu-box">
+			<h4>Filter Difficulty</h4>
+			<div class="clearfix"></div>
+		</div>
+	</a>
 </div>
 
-
-<div class="workout-menu-box">
-    <a href="#">
-        <div class="workout-menu-img">
-            <?php include('partials/base64/random-menu.php'); ?>
-        </div>
-    <div class="workout-menu-desc">
-        <h3>Whatever</h3>
-            <p class="workout-detail">
-                Muscle group: Abdominals </br> </br>
-				Category: Home workout </br> </br>
-				Difficulty: Begginer 
-            </p>
-    </div>
-        <div class="clearfix"></div>
-    </a>
-</div>
-
-<div class="workout-menu-box">
-    <a href="#">
-        <div class="workout-menu-img">
-            <?php include('partials/base64/random-menu.php'); ?>
-        </div>
-    <div class="workout-menu-desc">
-        <h3>Whatever</h3>
-            <p class="workout-detail">
-                Muscle group: Biceps </br> </br>
-				Category: Home workout </br> </br>
-				Difficulty: Begginer 
-            </p>
-    </div>
-        <div class="clearfix"></div>
-    </a>
-</div>
-
-<div class="workout-menu-box">
-    <a href="#">
-        <div class="workout-menu-img">
-            <?php include('partials/base64/random-menu.php'); ?>
-        </div>
-    <div class="workout-menu-desc">
-        <h3>Whatever</h3>
-            <p class="workout-detail">
-                Muscle group: Quads </br> </br>
-				Category: Home workout </br> </br>
-				Difficulty: Begginer 
-            </p>
-    </div>
-        <div class="clearfix"></div>
-    </a>
-</div>
+<?php 
+	$query = "SELECT * FROM user_workouts";
+    $result = $conn->query($query);
+	if ($result->num_rows > 0) {
+		/* fetch associative array */
+		while($row = $result->fetch_assoc()) 
+		{
+			echo "<div class='workout-menu-box'>
+						<div class='workout-menu-img'>
+						<img src=data:image/gif;base64," .$row["user_image"]. " alt='random-menu1' class='img-responsive img-curve'>                  
+						</div>
+						<div class='workout-menu-desc'>
+								<h3>" .$row["user_name"]. "</h3>
+							<p class='workout-detail'>
+								Muscle group: ".$row["user_category"]."</br> </br>
+								Difficulty: ".$row["user_difficulty"]."</br> </br>
+								Description: ".$row["user_descr"]."</br> </br>
+								</p>
+						</div>
+							<div class='clearfix'></div>
+					</div>";
+		}
+	
+		/* free result set */
+		$result->free();
+	}
+?>
 
 <!-- Workouts section end -->
     <?php include('partials/footer.php'); ?>
+<?php
+	CloseCon($conn);
+?>
