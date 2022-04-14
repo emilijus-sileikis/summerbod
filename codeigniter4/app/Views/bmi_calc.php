cc8eba32e633a9f9fd096d0ab7865bd83a6265bb
Nazar <mamedov.nazar@gmail.com>
2022-04-14 03:11:36 +0300

    health tab css

--- codeigniter4/app/Views/bmi_calc.php
+++ codeigniter4/app/Views/bmi_calc.php
@@ -12,21 +12,20 @@ BMI is widely used as a general indicator of whether a person has a healthy body
 of BMI is used to categorize whether a person is underweight, normal weight, overweight, or obese depending on what range the value falls between. Being overweight or underweight
 can have significant health effects, so while BMI is an imperfect measure of healthy body weight, it is a useful indicator of whether any additional testing or action is required.
 </p>
-
-<h4 class="tech-text" style="padding-bottom: 1%">BMI Table For Adults:</h4>
 <p class="tech-text">BMI is calculated differently based on age and gender. The table for adults considers both males and females that are above the age of 20. Below you can see the BMI table for adults.</p>
-<table class="tech-text">
+<p class="tech-text" >As mentioned above, BMI value is different for Non-Adults. In addition, gender also has a small inpact for children and teenagers BMI. Below you can see the BMI table for girls younger than 21.</p>
+<p class="tech-text" >Younger males also have their own BMI scale which is slightly different from females below the age of 21. Below you can see the BMI table of males younger than 21.</p>
+<div class="diffe-menu-container">
+<div class="health-menu-box solid">
+<h4 style="padding-bottom: 1%">BMI Table For Adults:</h4>
+<table>
     <tr>
         <td class="table-name">Category</td>
         <td class="table-name">BMI range - kg/m2</td>
     </tr>
     <tr>
-        <td>Severe Thinness</td>
-        <td>< 16</td>
-    </tr>
-    <tr>
         <td>Underweight</td>
-        <td>from 16 to 18.5</td>
+        <td>< 16 to 18.5</td>
     </tr>
     <tr>
         <td>Normal Weight</td>
@@ -41,10 +40,10 @@ can have significant health effects, so while BMI is an imperfect measure of hea
         <td>> 29.9</td>
     </tr>
 </table>
-				
-<h4 class="tech-text" style="padding-bottom: 1%">BMI Table For Non-Adults (Girls):</h4>
-<p class="tech-text" >As mentioned above, BMI value is different for Non-Adults. In addition, gender also has a small inpact for children and teenagers BMI. Below you can see the BMI table for girls younger than 21.</p>
-<table class="tech-text">
+</div>
+<div class="health-menu-box solid">	
+<h4 style="padding-bottom: 1%">BMI Table For Non-Adults (Girls):</h4>
+<table>
     <tr>
         <td class="table-name-girls">Category</td>
         <td class="table-name-girls">BMI range - kg/m2</td>
@@ -66,10 +65,10 @@ can have significant health effects, so while BMI is an imperfect measure of hea
         <td>> 32.6</td>
     </tr>
 </table>
-				
-<h4 class="tech-text" style="padding-bottom: 1%">BMI Table For Non-Adults (Boys):</h4>
-<p class="tech-text" >Younger males also have their own BMI scale which is slightly different from females below the age of 21. Below you can see the BMI table of males younger than 21.</p>
-<table class="tech-text">
+</div>
+<div class="health-menu-box solid">		
+<h4 style="padding-bottom: 1%">BMI Table For Non-Adults (Boys):</h4>
+<table>
     <tr>
         <td class="table-name-boys">Category</td>
         <td class="table-name-boys">BMI range - kg/m2</td>
@@ -91,6 +90,9 @@ can have significant health effects, so while BMI is an imperfect measure of hea
         <td>> 31.1</td>
     </tr>
 </table>
+</div>
+</div>
+<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
 				
 <h4 class="tech-text" style="padding-bottom: 1%">Risks Associated With Being Overweight:</h4>
 <p class="tech-text" >Being overweight increases the risk of a number of serious diseases and health conditions. Below is a list of said risks, according to the Centers for Disease Control and Prevention (CDC):</p>
@@ -111,7 +113,7 @@ can have significant health effects, so while BMI is an imperfect measure of hea
 <li class="tech-text" style="margin-left: 150px;">Growth and development issues.</li>
 <li class="tech-text" style="margin-left: 150px;">Possible reproductive issues for women due to hormonal imbalance.</li>
 <li class="tech-text" style="margin-left: 150px;">Generally, an increased risk of mortality.</li>
-
+</div>
 
 
 <!-- Workouts section end -->
