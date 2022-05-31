de441abf404799d0183c0f1ee70ebbd8d040d5b6
Emilijus <emilijus.sileikis@mif.stud.vu.lt>
2022-05-31 18:03:53 +0300

    Injury treatment fill in

--- summerbod/app/Views/health/injury_treatment.php
+++ summerbod/app/Views/health/injury_treatment.php
@@ -1,19 +1,64 @@
 <?php echo view('partials/menuhealthnav'); ?>
 <!-- Health section start -->
-<link rel="stylesheet" href="<?php echo base_url('assets/css/techniques.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/health.css'); ?>">
 
 <div class="container">
 
     <h2 class="text-center header">Injury treatment</h2>
-    <h3 class="tech-text"> Health benefits </h3>
+    <h3 class="health-text"> How do they happen? </h3>
     
-    <p class="tech-text">
-        Running, lifting weights, doing fitness exercises, regardless of what you do, regular exercise and physical activity is the path to health and well-being. 
-        Exercise burns fat, builds muscle, lowers cholesterol, eases stress and anxiety and even lets us sleep better. But working out is not the only thing keeping you healthy. 
-        Every person must take care of their body not only by going to the gym and doing exercises, but also by doing those exercises correctly with a good form and caution 
-        in order to prevent injuries. However, no matter if you are a beginner, amateur or even a professional body builder injuries might happen. In this section, you can find more
-        information on how to calculate your body mass, prevent and treat injuries, get some posture advices and even see how to treat bad posture complications.
+    <p class="health-text">
+        A workout injury can happen to anyone, no matter their experience or fitness level. Sometimes even some simple exercises like running can cause an injury.
+        But you can significantly cut your risk of getting hurt by following certain workout precautions, doing a good warm up and other things that we recommend in our 
+        <a href="<?php echo base_url('public/injury_prevention'); ?>" style="font-weight: bold; color: black;">"Injury prevention"</a> page. Generally, workout injuries are 
+        commonly caused by overuse, direct impact, or the application of force that is greater than the body part can structurally withstand. In this page you will learn the most
+        common workout injuries and some advices on how to treat them.
     </p>
+
+    <br>
+
+    <h3 class="health-text-header"> Common injuries </h3>
+    
+    <p class="health-text" style="font-weight: bold;">Some of the more common sports injuries include:</p>
+    <li class="health-text">Ankle sprain – symptoms include pain, swelling and stiffness.</li>
+    <li class="health-text">Bruises – a blow can cause small bleeds into the skin.</li>
+    <li class="health-text">Cuts and abrasions – are usually caused by falls. The knees and hands are particularly prone.</li>
+    <li class="health-text">Dehydration – losing too much fluid can lead to heat exhaustion and heat stroke.</li>
+    <li class="health-text">Groin strain – symptoms include pain and swelling.</li>
+    <li class="health-text">Hamstring strain – symptoms include pain, swelling and bruising.</li>
+    <li class="health-text">Knee joint injuries – symptoms include pain, swelling and stiffness. The ligaments, tendons or cartilage can be affected.</li>
+    <li class="health-text">Stress fractures – particularly in the lower limbs. The impact of repeated jumping or running on hard surfaces can eventually stress and crack bone.</li>
+
+    <br>
+
+    <h3 class="health-text-header"> First aid for sprains, strains and joint injuries </h3>
+
+    <p class="health-text" style="font-weight: bold;">Suggestions on immediate treatment for sprains, strains and joint injuries, to prevent further damage include:</p>
+    <li class="health-text">Rest – keep the injured area supported and avoid using for 48-72 hours.</li>
+    <li class="health-text">Ice – apply ice to the injured area for 20 minutes every two hours for the first 48-72 hours.</li>
+    <li class="health-text">Compression – apply a firm elastic bandage over the area, extending above and below the painful site.</li>
+    <li class="health-text">Elevation – raise the injured area above the level of the heart at all times.</li>
+    <li class="health-text">Referral – as soon as possible, see a doctor.</li>
+
+    <br>
+
+    <p class="health-text" style="font-weight: bold;">What not to do!</p>
+    <li class="health-text">No Heat – heat will increase bleeding.</li>
+    <li class="health-text">No Alcohol – alcohol increases bleeding and swelling.</li>
+    <li class="health-text">No Running – running or exercise increases blood flow, delaying healing.</li>
+    <li class="health-text">No Massage – massage increases swelling and bleeding, also delaying healing.</li>
+
+    <br>
+
+    <h3 class="health-text-header"> Emergency situations </h3>
+
+    <p class="health-text" style="font-weight: bold;">Call an ambulance immediately for:</p>
+    <li class="health-text">Prolonged loss of consciousness.</li>
+    <li class="health-text">Neck or spine injuries.</li>
+    <li class="health-text">Broken bones.</li>
+    <li class="health-text">Eye injuries.</li>
+    <li class="health-text">Abdominal or back injuries.</li>
+
 </div>
 <!-- Workouts section end -->
 <?php echo view('partials/footer'); ?>
