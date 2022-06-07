9d7304c5def08c650012d3054126f4454b0d7c77
Patrikas <patrik.voronovic@mif.stud.vu.lt>
2022-06-07 20:53:17 +0300

    Last generate fixes
    css and js

--- summerbod/assets/js/generate.js
+++ summerbod/assets/js/generate.js
@@ -7,18 +7,17 @@
     document.body.scrollTop = 1000; // For Safari
     document.documentElement.scrollTop = 1000; // For Chrome, Firefox, IE and Opera
   } 
-// Nuo cia pasiziurek
-  let restart = sessionStorage.getItem("restart") //pasiimam kintamaji
-  resetBtn.addEventListener("click", () => { //jei paspaudziam and refresh mygtuko
-  sessionStorage.setItem("restart", "true") //padarom kad tipo restartino page
-  location.reload() //pareloadinam page
+
+  let restart = sessionStorage.getItem("restart") 
+  resetBtn.addEventListener("click", () => { 
+  sessionStorage.setItem("restart", "true") 
+  location.reload() 
   })
   
-  window.onload = () => { //kvieciam tik kai vyksta reloadas
+  window.onload = () => {
       if(restart){ 
           window.scrollTo(0, document.body.scrollHeight);
           document.getElementById("boxwithworkouts").style.display = "block";
-          sessionStorage.clear() // cleans all the session storage
+          sessionStorage.clear()
       }
   };
-  //istrink komentarus po to
