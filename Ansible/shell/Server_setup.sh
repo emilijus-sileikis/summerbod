0f7efb589d1b2abd3568f754b2b611abfb0fb1e2
Emilijus <emilijus.sileikis@mif.stud.vu.lt>
2022-06-10 09:31:11 +0300

    Added allow password verification

--- Ansible/shell/Server_setup.sh
+++ Ansible/shell/Server_setup.sh
@@ -40,4 +40,7 @@ fi
 # Adjust SSH configuration ownership and permissions
 chmod 0700 "${home_directory}/.ssh"
 chmod 0600 "${home_directory}/.ssh/authorized_keys"
-chown --recursive "${USERNAME}":"${USERNAME}" "${home_directory}/.ssh"
+chown --recursive "${USERNAME}":"${USERNAME}" "${home_directory}/.ssh
+
+# Allow Password verification
+sudo sed -i '/PasswordAuthentication/s/^#//g' /etc/ssh/sshd_config
